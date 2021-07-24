e3579c8527a8c89746ab085f34c46a380f2acb76
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-07-24 17:02:36 -0300

    reset css

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-// import './style.css'
+import './src/styles/settings/colors.css'
+import '/src/styles/generic/reset.css'
+import './src/styles/elements/base.css'
+
 import CardGame from './src/components/CardGame'
+
 
 const $root = document.querySelector('#root')
 const $htmlCardGame = CardGame()
